89f739d5c98e7521e205864ae59f096a1c5f8dd7
desodre <jhonathacirilo9000@gmail.com>
2026-05-18 03:31:34 -0400

    (refac) removed many heavy dependences and useless project generated files.

--- app/src/main/java/com/example/phantom_agent/MainActivity.kt
+++ app/src/main/java/com/example/phantom_agent/MainActivity.kt
@@ -1,22 +1,10 @@
 package com.example.phantom_agent
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import com.example.phantom_agent.theme.PhantomAgentTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : Activity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-
-    enableEdgeToEdge()
-    setContent {
-      PhantomAgentTheme { Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) { MainNavigation() } }
-    }
   }
 }
